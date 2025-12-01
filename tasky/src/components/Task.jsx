803283125ab63d18d5f3b0891e1c4394098bb162
Dion Kennedy <20101554@mail.wit.ie>
2025-12-01 11:09:05 +0000

    Mark task as done

--- tasky/src/components/Task.jsx
+++ tasky/src/components/Task.jsx
@@ -3,11 +3,12 @@ import React from 'react';
 const Task = (props) => {
     
     return (
-        <div className="card">
+        <div className="card" style={{backgroundColor: props.done ? 'lightgrey' : '#5bb4c4'}}>
             <p className="title">{props.title}</p>
             <p>Due: {props.deadline}</p>
             <p className="description">{props.description}</p>
             <p className="priority">{props.priority}</p>
+            <button onClick={props.markDone} className='doneButton'>Done</button>
         </div>
     )
 }
